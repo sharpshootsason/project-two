5089d62e50e1937806d5f1a8c77582b3f2c992e1
sharpshootsason <sakshi.rakesh.gosain@gmail.com>
2023-03-31 16:37:31 -0400

    HELP

---
+++ main.js
@@ -0,0 +1,30 @@
+let color = "black"
+
+function populateBoard(size) {
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat($(size) , 1fr)`;
+board.style.gridTemplateRows = `repeat($(size) , 1fr)`;
+
+let amount = size * size;
+for (let i = 0; i < amount; i++) {
+let square = document.createElement("div");
+square.addEventListener("mouseover",(colorSquare));
+square.style.backgroundColor = "blue";
+board.insertAdjacentElement("beforeend", square);
+ }
+}
+
+populateBoard(16)
+
+
+function changeSize(input) {
+    populateBoard(input)
+}
+
+function colorSquare() {
+    this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+    color=choice;
+}
