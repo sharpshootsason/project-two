ac21fa412c96375a714041350dc74c7d0c012dab
sharpshootsason <sakshi.rakesh.gosain@gmail.com>
2023-04-03 17:41:44 -0400

    fixed the grid

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ let color = "black"
 
 function populateBoard(size) {
 let board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat($(size) , 1fr)`;
-board.style.gridTemplateRows = `repeat($(size) , 1fr)`;
+board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
 let amount = size * size;
 for (let i = 0; i < amount; i++) {
 let square = document.createElement("div");
 square.addEventListener("mouseover",(colorSquare));
-square.style.backgroundColor = "blue";
+square.style.backgroundColor = "white";
 board.insertAdjacentElement("beforeend", square);
  }
 }
 
-populateBoard(16)
+populateBoard(16);
 
 
 function changeSize(input) {
